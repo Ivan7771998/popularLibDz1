794c62d3f63190dc76a235793b4b440e2fcd47ed
Popov Ivan <>
2020-07-27 13:34:02 +0300

    finish

--- app/src/main/java/com/dev777popov/popularlibdz1/MainActivity.kt
+++ app/src/main/java/com/dev777popov/popularlibdz1/MainActivity.kt
@@ -2,10 +2,30 @@ package com.dev777popov.popularlibdz1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        rxDemonstration()
+    }
+
+    private fun rxDemonstration() {
+        inputText.addTextChangedListener( object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                textView.text = p0
+            }
+        })
     }
 }
